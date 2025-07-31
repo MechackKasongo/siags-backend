0334161c011eba3d7fce9352b7ba3c4345eac56e
mechakkasongo <20kk185@esisalama.org>
2025-07-31 16:20:15 +0200

    nouvel commit

---
+++ src/main/java/com/hgs/patient/siags_backend/SiagsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hgs.patient.siags_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiagsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiagsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgs/patient/siags_backend/SiagsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgs.patient.siags_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiagsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
